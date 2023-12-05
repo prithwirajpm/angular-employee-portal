64ff3b5c125da2e039052d584abe7f79f4d3afd1
prithwirajpm <wt.prithwiraj@gmail.com>
2023-12-05 13:01:30 +0530

    set Login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', loadChildren: () => import('./modules/users/users.module').then(m => m.UsersModule) },
+  // http://localhost:4200/
+  {
+    path:'',component:LoginComponent
+  },
+  // http://localhost:4200/dashboard
+  {
+    path:'dashboard',component:HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
